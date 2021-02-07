1126d3d81740834b8b50adf3774105a071dff18c
ishimori <ishimori_yuzuru@probsc.jp>
2021-02-07 13:51:30 +0900

    11回まで終了。 Warning: Each child in a list should have a unique "key" prop.
    Check the render method of `SideList`. See https://reactjs.org/link/warning-keys for more information.

--- src/apis/index.js
+++ src/apis/index.js
@@ -6,15 +6,34 @@ const youtube = axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3"
 });
 
+const params = {
+  part: "snippet",
+  maxResults: 20,
+  key: KEY,
+  regionCode: "JP",
+  type: "video"
+};
 export const fetchPopularData = async () => {
   return await youtube.get("/videos", {
     params: {
-      part: "snippet",
-      maxResults: 20,
-      key: KEY,
-      regionCode: "JP",
-      type: "video",
-      chart: "mostPopular"
+      ...params,
+      chart: "mostpopular"
+    }
+  });
+};
+export const fetchSelectedData = async (id) => {
+  return await youtube.get("/videos", {
+    params: {
+      ...params,
+      id
+    }
+  });
+};
+export const fetchRelatedData = async (id) => {
+  return await youtube.get("/search", {
+    params: {
+      ...params,
+      relatedToVideoId: id
     }
   });
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,18 @@
 import React, { createContext, useReducer } from "react";
 
 const initialState = {
-  popular: []
+  popular: [],
+  related: [],
+  selected: {}
 };
 const reducer = (state, action) => {
   switch (action.type) {
     case "SET_POPULAR":
-      return { popular: action.payload.popular };
+      return { ...state, popular: action.payload.popular };
+    case "SET_SELECTED":
+      return { ...state, selected: action.payload.selected };
+    case "SET_RELATED":
+      return { ...state, related: action.payload.related };
     default:
       return state;
   }
